9a32b8d1d15bb622950151cae4ed921cdb7c8095
MariPinas <marianapls2003@gmail.com>
2025-03-07 04:58:56 -0300

    :sparkles: ajustado a sobreposicao do teclado com as decoracoes - login v0.4

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,10 @@ import {
   TouchableOpacity,
   StyleSheet,
   Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Platform,
 } from "react-native";
 import { useRouter } from "expo-router";
 import EmailIcon from "@/assets/svg/emailIcon";
@@ -17,7 +21,6 @@ const LoginScreen = () => {
   const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const router = useRouter();
-  
 
   const handleLogin = () => {
     // logica criar conta enviar pro back
@@ -31,85 +34,103 @@ const LoginScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("../assets/images/logo.png")}
-        style={{ width: 150, height: 150, marginBottom: 20 }}
-      />
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.innerContainer}>
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{ width: 150, height: 150, marginBottom: 20 }}
+          />
 
-      {/*Email*/}
-      <View style={styles.input}>
-        <TextInput
-          placeholder="E-mail"
-          placeholderTextColor={"#3333338e"}
-          value={email}
-          onChangeText={setEmail}
-        />
-        <EmailIcon style={styles.icons} />
-      </View>
+          {/*Email*/}
+          <View style={styles.inputWrapper}>
+            <TextInput
+              placeholder="E-mail"
+              placeholderTextColor={"#3333338e"}
+              value={email}
+              onChangeText={setEmail}
+              style={styles.input}
+            />
+            <EmailIcon style={styles.icons} />
+          </View>
 
-      {/*Senha */}
-      <View style={styles.input}>
-        <TextInput
-          placeholder="Senha"
-          placeholderTextColor={"#3333338e"}
-          value={password}
-          onChangeText={setPassword}
-          multiline={false}
-          numberOfLines={1}
-          secureTextEntry={!isPasswordVisible}
-        />
-        <TouchableOpacity onPress={ativarVisibilidadeSenha} style={styles.icons}>
-          {isPasswordVisible ? (
-            <EyeOpen/>
-          ) : (
-            <EyeClosed/>
-          )}
-        </TouchableOpacity>
-      </View>
-        
+          {/*Senha */}
+          <View style={styles.inputWrapper}>
+            <TextInput
+              placeholder="Senha"
+              placeholderTextColor={"#3333338e"}
+              value={password}
+              onChangeText={setPassword}
+              multiline={false}
+              numberOfLines={1}
+              secureTextEntry={!isPasswordVisible}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              onPress={ativarVisibilidadeSenha}
+              style={styles.icons}
+            >
+              {isPasswordVisible ? <EyeOpen /> : <EyeClosed />}
+            </TouchableOpacity>
+          </View>
 
-      {/*Esqueci minha senha - criar a page esqueci senha*/}
-      <TouchableOpacity>
-        <Text style={styles.forgotPasswordText}>Esqueci minha senha.</Text>
-      </TouchableOpacity>
+          {/*Esqueci minha senha - criar a page esqueci senha*/}
+          <TouchableOpacity>
+            <Text style={styles.forgotPasswordText}>Esqueci minha senha.</Text>
+          </TouchableOpacity>
 
-      {/*Login */}
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
+          {/*Login */}
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
 
-      {/* Link cadastrar */}
-      <TouchableOpacity onPress={() => router.push("/register")} style={styles.texto}>
-        <Text style={styles.loginText}>Não possui uma conta?</Text>
-        <Text style={styles.cadText}>Cadastre-se</Text>
-      </TouchableOpacity>
-
-      /* Decoracao */
-    <View style={styles.circleTopRight}/>
-    <View style={styles.circleBottomLeft} />
-    </View>
+          {/* Link cadastrar */}
+          <TouchableOpacity
+            onPress={() => router.push("/register")}
+            style={styles.texto}
+          >
+            <Text style={styles.loginText}>Não possui uma conta?</Text>
+            <Text style={styles.cadText}>Cadastre-se</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+      {/* Decoracao */}
+      <View style={styles.circleTopRight} />
+      <View style={styles.circleBottomLeft} />
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#2C3E50",
+  },
+  innerContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor: "#2C3E50",
+    position: "relative",
   },
-  input: {
+  inputWrapper: {
+    width: "90%",
+    marginVertical: 10,
+    position: "relative",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "90%",
-    paddingRight: 30,
+  },
+  input: {
+    width: "100%",
+    paddingRight: 40,
     paddingLeft: 20,
     height: 50,
     padding: 5,
-    marginVertical: 20,
+    marginVertical: 15,
     backgroundColor: "#F4F4F4F4",
     borderRadius: 11,
   },
@@ -152,31 +173,30 @@ const styles = StyleSheet.create({
   iconContainer: {
     paddingRight: 10,
   },
-  circleBottomLeft:{
+  circleBottomLeft: {
     position: "absolute",
     top: 0,
     right: 0,
-    width: 343, 
-    height: 52, 
-    borderRadius:51,
-    borderBottomRightRadius: 0, 
+    width: 343,
+    height: 52,
+    borderRadius: 51,
+    borderBottomRightRadius: 0,
     borderTopEndRadius: 0,
-    borderTopStartRadius:0,
+    borderTopStartRadius: 0,
     backgroundColor: "#f4f4f4",
   },
-  circleTopRight:{
+  circleTopRight: {
     position: "absolute",
     bottom: 0,
     left: 0,
     width: 343,
     height: 52,
-    borderRadius:51,
-    borderBottomLeftRadius: 0, 
-    borderBottomRightRadius: 0, 
-    borderTopStartRadius:0,
+    borderRadius: 51,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopStartRadius: 0,
     backgroundColor: "#f4f4f4",
   },
-
 });
 
 export default LoginScreen;
